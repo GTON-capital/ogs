33cd466a05a93153c668976b816293c4d18aaed1
Slava Startsev <slava.startsev@tuta.io>
2022-11-28 15:04:33 +0300

    feature/removeLPFee (#3)
    * chore: set fi to 1 to take 100 percent fee
    
    * chore: rename OGS -> OGX
    
    * chore: add ogx keyword
    
    * chore: optimise for gas savings

--- contracts/OGXERC20.sol
+++ contracts/OGXERC20.sol
@@ -1,13 +1,13 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IOGSERC20.sol';
+import './interfaces/IOGXERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract OGSERC20 is IOGSERC20 {
+contract OGXERC20 is IOGXERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'OGS LP';
-    string public constant symbol = 'OGS-LP';
+    string public constant name = 'OGX LP';
+    string public constant symbol = 'OGX-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
@@ -79,7 +79,7 @@ contract OGSERC20 is IOGSERC20 {
     }
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, 'OGS: EXPIRED');
+        require(deadline >= block.timestamp, 'OGX: EXPIRED');
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
@@ -88,7 +88,7 @@ contract OGSERC20 is IOGSERC20 {
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, 'OGS: INVALID_SIGNATURE');
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'OGX: INVALID_SIGNATURE');
         _approve(owner, spender, value);
     }
 }
--- test/OGXFactory.spec.ts
+++ test/OGXFactory.spec.ts
@@ -7,7 +7,7 @@ import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
 import { getCreate2Address } from './shared/utilities'
 import { factoryFixture } from './shared/fixtures'
 
-import OGSPair from '../build/OGSPair.json'
+import OGXPair from '../build/OGXPair.json'
 
 chai.use(solidity)
 
@@ -16,7 +16,7 @@ const TEST_ADDRESSES: [string, string] = [
   '0x2000000000000000000000000000000000000000'
 ]
 
-describe('OGSFactory', () => {
+describe('OGXFactory', () => {
   const provider = new MockProvider({
     hardfork: 'istanbul',
     mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
@@ -38,20 +38,20 @@ describe('OGSFactory', () => {
   })
 
   async function createPair(tokens: [string, string]) {
-    const bytecode = `0x${OGSPair.evm.bytecode.object}`
+    const bytecode = `0x${OGXPair.evm.bytecode.object}`
     const create2Address = getCreate2Address(factory.address, tokens, bytecode)
     await expect(factory.createPair(...tokens))
       .to.emit(factory, 'PairCreated')
       .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, bigNumberify(1))
 
-    await expect(factory.createPair(...tokens)).to.be.reverted // OGS: PAIR_EXISTS
-    await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted // OGS: PAIR_EXISTS
+    await expect(factory.createPair(...tokens)).to.be.reverted // OGX: PAIR_EXISTS
+    await expect(factory.createPair(...tokens.slice().reverse())).to.be.reverted // OGX: PAIR_EXISTS
     expect(await factory.getPair(...tokens)).to.eq(create2Address)
     expect(await factory.getPair(...tokens.slice().reverse())).to.eq(create2Address)
     expect(await factory.allPairs(0)).to.eq(create2Address)
     expect(await factory.allPairsLength()).to.eq(1)
 
-    const pair = new Contract(create2Address, JSON.stringify(OGSPair.abi), provider)
+    const pair = new Contract(create2Address, JSON.stringify(OGXPair.abi), provider)
     expect(await pair.factory()).to.eq(factory.address)
     expect(await pair.token0()).to.eq(TEST_ADDRESSES[0])
     expect(await pair.token1()).to.eq(TEST_ADDRESSES[1])
@@ -68,19 +68,19 @@ describe('OGSFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2502486)
+    expect(receipt.gasUsed).to.eq(2494864)
   })
 
   it('setFeeTo', async () => {
-    await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('OGS: FORBIDDEN')
+    await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('OGX: FORBIDDEN')
     await factory.setFeeTo(wallet.address)
     expect(await factory.feeTo()).to.eq(wallet.address)
   })
 
   it('setFeeToSetter', async () => {
-    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('OGS: FORBIDDEN')
+    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('OGX: FORBIDDEN')
     await factory.setFeeToSetter(other.address)
     expect(await factory.feeToSetter()).to.eq(other.address)
-    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('OGS: FORBIDDEN')
+    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('OGX: FORBIDDEN')
   })
 })
